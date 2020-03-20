06fde5ef5ceb0c15739388b9b75d036270fd1c5c
Royer <49170849+royerjmasache@users.noreply.github.com>
2020-03-19 23:57:08 -0500

    Proyecto final con interfaz gráfica de usuario POO

---
+++ poo2020/src/proyectopoo/Product.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+/**
+ *
+ * @author royerjmasache
+ */
+public class Product {
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the measure
+     */
+    public String getMeasure() {
+        return measure;
+    }
+
+    /**
+     * @param measure the measure to set
+     */
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return the value
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+    private String name;
+    private String measure;
+    private int quantity;
+    private double value;
+    
+    // Método constructor para la clase Product
+    public Product(String name, String measure, int quantity, double value) {
+        this.name = name;
+        this.measure = measure;
+        this.quantity = quantity;
+        this.value = value;
+    }
+    
+}
